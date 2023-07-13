0b9ec89eb77436ef0a8a6ee0d72b2b904176d10e
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 <dohyun911@hanmail.net>
2023-07-14 03:16:48 +0900

    feature: MapTraffic usEffect 첫 마운트시에만 발생하는 코드와 아닌 것 코드 정리 및 mapInstance 첫 렌더링 시 null오류  옵셔널 체이닝 사용

--- src/components/sub/MapTraffic.js
+++ src/components/sub/MapTraffic.js
@@ -7,28 +7,29 @@ function MapTraffic() {
 	const map = useRef(null);
 	const { kakao } = window;
 	const [Traffic, setTraffic] = useState(false);
-
+	const [Location, setLocation] = useState(null);
 	const option = {
 		center: new kakao.maps.LatLng(37.42432343572819, 126.88286963577373), // 지도의 중심좌표
 		level: 3, // 지도의 확대 레벨
 	};
+	const imgSrc = `${process.env.PUBLIC_URL}/img/marker1.png`;
+	const imgSize = new kakao.maps.Size(110, 100);
+	const imgPos = { offset: new kakao.maps.Point(56, 80) };
+	const markerImage = new kakao.maps.MarkerImage(imgSrc, imgSize, imgPos);
+	const marker = new kakao.maps.Marker({
+		position: option.center,
+		image: markerImage,
+	});
 
 	useEffect(() => {
 		const mapInstance = new kakao.maps.Map(map.current, option);
 
-		const imgSrc = `${process.env.PUBLIC_URL}/img/marker1.png`;
-		const imgSize = new kakao.maps.Size(110, 100);
-		const imgPos = { offset: new kakao.maps.Point(56, 80) };
-		const markerImage = new kakao.maps.MarkerImage(imgSrc, imgSize, imgPos);
-
-		const marker = new kakao.maps.Marker({
-			position: option.center,
-			image: markerImage,
-		});
-
 		marker.setMap(mapInstance);
+		setLocation(mapInstance);
+	}, []);
 
-		Traffic ? mapInstance.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC) : mapInstance.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+	useEffect(() => {
+		Traffic ? Location?.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC) : Location?.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
 	}, [Traffic]);
 
 	return (
@@ -56,7 +57,7 @@ function MapTraffic() {
 							</div>
 							<div className='rightside'>
 								<button onClick={() => setTraffic(!Traffic)}>
-									{Traffic ? '교통정보 ON' : '교통정보 OFF'} <FontAwesomeIcon icon={faCaretDown} />
+									{Traffic ? '교통정보 OFF' : '교통정보 ON'} <FontAwesomeIcon icon={faCaretDown} />
 								</button>
 							</div>
 						</div>
