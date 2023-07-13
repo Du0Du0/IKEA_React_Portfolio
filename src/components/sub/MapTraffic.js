8d9226ab71634e7eb7a164588bfd75f0e30c065c
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 <dohyun911@hanmail.net>
2023-07-14 01:58:10 +0900

    feature: MapTraffic 기본 지도 인스턴스 생성

---
+++ src/components/sub/MapTraffic.js
@@ -0,0 +1,50 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEarthAsia } from '@fortawesome/free-solid-svg-icons';
+import { useState, useRef } from 'react';
+import { useEffect } from 'react';
+
+function MapTraffic() {
+	const map = useRef(null);
+	const { kakao } = window;
+
+	const option = {
+		center: new kakao.maps.LatLng(37.42432343572819, 126.88286963577373), // 지도의 중심좌표
+		level: 3, // 지도의 확대 레벨
+	};
+
+	useEffect(() => {
+		new kakao.maps.Map(map.current, option);
+	}, []);
+
+	return (
+		<div className='locationContainer'>
+			<div className='titWrap'>
+				<h3>오시는 길</h3>
+
+				<div className='tabMenu'>
+					<ul>
+						<li className='active'>주차안내</li>
+						<li>지하철</li>
+						<li>버스</li>
+					</ul>
+				</div>
+
+				<div className='mapContainer'>
+					<div className='leftInfo'>
+						<div id='map' ref={map}>
+							map
+						</div>
+						<div className='addressDesc'>
+							<FontAwesomeIcon icon={faEarthAsia} className='loactionIcon' />
+							주소
+						</div>
+					</div>
+
+					<div className='rightInfo'>안녕</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default MapTraffic;
--- src/components/sub/StoreName.js
+++ src/components/sub/StoreName.js
@@ -4,6 +4,9 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import moment from 'moment';
 import Rain from './Rain';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
 
 function StoreName() {
 	const [City, setCity] = useState('Goyang-si');
@@ -11,7 +14,7 @@ function StoreName() {
 	const [Icon, setIcon] = useState('');
 	const [Temp, setTemp] = useState(0);
 	const [value, onChange] = useState(new Date());
-
+	const path = process.env.PUBLIC_URL;
 	// 지점명: 영어 도시 이름 => 한글로 변경
 	const getCityLocation = (City) => {
 		switch (City) {
@@ -27,6 +30,26 @@ function StoreName() {
 			case 'Busan':
 				console.log('동부산점');
 				return <h2>동부산점</h2>;
+			default:
+				return null;
+		}
+	};
+
+	//지점명 :영어 도시 이름 => 한글로 변경 (맨 하단 배너)
+	const getCityNameBanner = (City) => {
+		switch (City) {
+			case 'Goyang-si':
+				console.log('고양점');
+				return <p>고양점</p>;
+			case 'Yongin':
+				console.log('기흥점');
+				return <p>기흥점</p>;
+			case 'Gyeonggi-do':
+				console.log('광명점');
+				return <p>광명점</p>;
+			case 'Busan':
+				console.log('동부산점');
+				return <p>동부산점</p>;
 			default:
 				return null;
 		}
@@ -53,9 +76,9 @@ function StoreName() {
 		}
 	};
 
-	useEffect(() => {
-		fetchWeather();
-	}, [City]);
+	// useEffect(() => {
+	// 	fetchWeather();
+	// }, [City]);
 
 	//휴점
 	const holidayMarks = ['11-07-2023'];
@@ -79,92 +102,105 @@ function StoreName() {
 	];
 
 	return (
-		<div className='topPopUp'>
-			{/* 상단 안내 소개 배너*/}
-			<div className='upperBanner'>
-				<p>김도현님, 이케아 홈페이지에 오신걸 환영합니다.</p>
+		<>
+			<div className='topBgWrap'>
+				<img src={path + '/img/ikeaBuilding.png'} />
 			</div>
-			{/* 상단 매장지점 정보란 / 스케쥴 / 달력 표시 */}
 
-			<div className='storeListContainer'>
-				<div className='leftContainer'>
-					<div className='storeNameWrap'>
-						<h2>{getCityLocation(City)}</h2>
-						<select
-							name='storeName'
-							id='storeName'
-							onChange={(e) => {
-								setCity(e.target.value);
-							}}
-						>
-							<option value='Goyang'>고양점</option>
-							<option value='Yongin'>기흥점</option>
-							<option value='Gyeonggi-do'>광명점</option>
-							<option value='Busan'>동부산점</option>
-						</select>
-					</div>
-					<div className='weatherWrap'>
-						{/* {Weather} */}
-						<img src={` https://openweathermap.org/img/wn/${Icon}@2x.png`} />
-						<div className='temperature'>
-							<p>{Temp}</p>
+			<div className='topPopUp'>
+				{/* 상단 안내 소개 배너*/}
+
+				<div className='upperBanner'>
+					<p>김도현님, 이케아 홈페이지에 오신걸 환영합니다.</p>
+				</div>
+
+				{/* 상단 매장지점 정보란 / 스케쥴 / 달력 표시 */}
+
+				<div className='storeListContainer'>
+					<div className='leftContainer'>
+						<div className='storeNameWrap'>
+							<h2>{getCityLocation(City)}</h2>
+							<select
+								name='storeName'
+								id='storeName'
+								onChange={(e) => {
+									setCity(e.target.value);
+								}}
+							>
+								<option value='Goyang'>고양점</option>
+								<option value='Yongin'>기흥점</option>
+								<option value='Gyeonggi-do'>광명점</option>
+								<option value='Busan'>동부산점</option>
+							</select>
+						</div>
+						<div className='weatherWrap'>
+							{/* {Weather} */}
+							<img src={` https://openweathermap.org/img/wn/${Icon}@2x.png`} />
+							<div className='temperature'>
+								<p>{Temp}</p>
+							</div>
+						</div>
+						<div className='selectStoreDesc'>
+							<p>
+								오늘은 10:30부터 20:00까지
+								<br /> 정상 영업합니다.
+							</p>
 						</div>
 					</div>
-					<div className='selectStoreDesc'>
-						<p>
-							오늘은 10:30부터 20:00까지
-							<br /> 정상 영업합니다.
-						</p>
-					</div>
-				</div>
 
-				<div className='middleContainer'>
-					<ul>
-						<li>휴점</li>
-						<li>연장영업</li>
-						<li>오늘</li>
-						<li>1</li>
-						<li>1</li>
-						<li>1</li>
-						<li>1</li>
-					</ul>
-					<button>쇼핑 정보 바로가기</button>
-				</div>
-
-				<div className='rightContainer'>
-					<Calendar
-						onChange={onChange}
-						value={value}
-						locale='en-EN'
-						formatDay={(locale, date) => date.toLocaleString('en', { day: 'numeric' })}
-						selectRange={false}
-						next2Label={null}
-						prev2Label={null}
-						showNeighboringMonth={false}
-						tileClassName={({ date, view }) => {
-							if (holidayMarks.find((x) => x === moment(date).format('DD-MM-YYYY'))) {
-								return 'highlight';
-							}
-							if (openLongMarks.find((x) => x === moment(date).format('DD-MM-YYYY'))) {
-								return 'openLong';
-							}
-						}}
-					/>
-					<div className='dateDesc'>
+					<div className='middleContainer'>
 						<ul>
-							<li>휴점</li>
-							<li>연장영업</li>
-							<li>오늘</li>
+							<li>
+								<FontAwesomeIcon icon={faPhone} className='fontAwesomeIcon' />
+								1670-4532
+							</li>
+							<li>
+								<FontAwesomeIcon icon={faClock} className='fontAwesomeIcon' />
+								정상영업 10:30 ~ 20:00
+							</li>
+							<li>
+								<FontAwesomeIcon icon={faClock} className='fontAwesomeIcon' />
+								연장영업 10:30 ~ 20:30
+							</li>
 						</ul>
+						<button>쇼핑 정보 바로가기</button>
+					</div>
+
+					<div className='rightContainer'>
+						<Calendar
+							onChange={onChange}
+							value={value}
+							locale='en-EN'
+							formatDay={(locale, date) => date.toLocaleString('en', { day: 'numeric' })}
+							selectRange={false}
+							next2Label={null}
+							prev2Label={null}
+							showNeighboringMonth={false}
+							tileClassName={({ date, view }) => {
+								if (holidayMarks.find((x) => x === moment(date).format('DD-MM-YYYY'))) {
+									return 'highlight';
+								}
+								if (openLongMarks.find((x) => x === moment(date).format('DD-MM-YYYY'))) {
+									return 'openLong';
+								}
+							}}
+						/>
+						<div className='dateDesc'>
+							<ul>
+								<li>휴점</li>
+								<li>연장영업</li>
+								<li>오늘</li>
+							</ul>
+						</div>
 					</div>
 				</div>
-			</div>
 
-			{/* 하단 요약 배너 */}
-			<div className='descBanner'>
-				<p>HOME / 점포안내 / 고양 / 한눈에 보기</p>
+				{/* 하단 요약 배너 */}
+				<div className='descBanner'>
+					<p>HOME</p>/<p>점포안내</p>/<p>{getCityNameBanner(City)}</p>/<p>한눈에 보기</p>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
 
