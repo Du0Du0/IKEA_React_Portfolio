4917e194d2dca569f8fbbef48795cd9462bbea2f
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 <dohyun911@hanmail.net>
2023-07-14 18:33:04 +0900

    feature: contact 기본 폼메일 적용

--- src/components/sub/Contact.js
+++ src/components/sub/Contact.js
@@ -4,11 +4,27 @@ import { faPrint, faPhone, faMobile } from '@fortawesome/free-solid-svg-icons';
 import { faClock } from '@fortawesome/free-regular-svg-icons';
 import StoreName from './StoreName';
 import MapTraffic from './MapTraffic';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 function Contact() {
 	const [City, setCity] = useState('Goyang-si');
 	const [Index, setIndex] = useState(0);
+	const form = useRef(null);
+
+	//폼메일 전송 함수
+	const sendEmail = (e) => {
+		e.preventDefault();
+
+		emailjs.sendForm('service_zxmrkcb', 'template_gtne1ai', form.current, 'PP62qMtzJtTKVyq').then(
+			(result) => {
+				console.log(result.text);
+			},
+			(error) => {
+				console.log(error.text);
+			}
+		);
+	};
 
 	return (
 		<LayoutNone type={''} name1={'contact'}>
@@ -21,17 +37,23 @@ function Contact() {
 						<h2>문의하기</h2>
 					</div>
 				</div>
-				<div className='formMailWrap'>
-					<div className='formTopWrap'>
-						<input type='text' placeholder='이름을 입력해주세요.' />
-						<input type='text' placeholder='이메일을 입력해주세요.' />
-					</div>
-					<div className='formMiddleWrap'>
-						<textarea name='' id='' cols='100' rows='5' placeholder='내용을 입력해주세요.' />
-					</div>
-					<div className='formBottomWrap'>
-						<button>보내기</button>
-					</div>
+				<div className='formMailWrap' id='formBox'>
+					<form ref={form} onSubmit={sendEmail}>
+						<div className='formTopWrap'>
+							<label>Name</label>
+							<input type='text' name='name' placeholder='이름을 입력해주세요.' />
+							<label>Email</label>
+							<input type='text' name='email' placeholder='이메일을 입력해주세요.' />
+						</div>
+						<div className='formMiddleWrap'>
+							<label>Message</label>
+							<textarea name='message' id='' cols='100' rows='5' placeholder='내용을 입력해주세요.' />
+						</div>
+						<div className='formBottomWrap'>
+							<input type='submit' value='Send' className='submitBtn' />
+							보내기
+						</div>
+					</form>
 				</div>
 			</div>
 
