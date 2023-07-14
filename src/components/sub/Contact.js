33d98a2e60065d5831352b9c4214a110170e77d7
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 <dohyun911@hanmail.net>
2023-07-14 18:49:51 +0900

    feature: contact 메일 전송시 성공메세지 띄우고 input 초기화

--- src/components/sub/Contact.js
+++ src/components/sub/Contact.js
@@ -11,17 +11,26 @@ function Contact() {
 	const [City, setCity] = useState('Goyang-si');
 	const [Index, setIndex] = useState(0);
 	const form = useRef(null);
+	const inputName = useRef('');
+	const inputEmail = useRef('');
+	const inputMsg = useRef('');
+	const [Success, setSuccess] = useState(false);
 
 	//폼메일 전송 함수
 	const sendEmail = (e) => {
 		e.preventDefault();
 
-		emailjs.sendForm('service_zxmrkcb', 'template_gtne1ai', form.current, 'PP62qMtzJtTKVyq').then(
+		emailjs.sendForm('service_zxmrkcb', 'template_gtne1ai', form.current, '-2LQanwpgbk-qnYZG').then(
 			(result) => {
 				console.log(result.text);
+				setSuccess(true);
+				inputName.current.value = '';
+				inputEmail.current.value = '';
+				inputMsg.current.value = '';
 			},
 			(error) => {
 				console.log(error.text);
+				setSuccess(false);
 			}
 		);
 	};
@@ -40,18 +49,15 @@ function Contact() {
 				<div className='formMailWrap' id='formBox'>
 					<form ref={form} onSubmit={sendEmail}>
 						<div className='formTopWrap'>
-							<label>Name</label>
-							<input type='text' name='name' placeholder='이름을 입력해주세요.' />
-							<label>Email</label>
-							<input type='text' name='email' placeholder='이메일을 입력해주세요.' />
+							<input type='text' name='name' placeholder='이름을 입력해주세요.' ref={inputName} />
+
+							<input type='text' name='email' placeholder='이메일을 입력해주세요.' ref={inputEmail} />
 						</div>
 						<div className='formMiddleWrap'>
-							<label>Message</label>
-							<textarea name='message' id='' cols='100' rows='5' placeholder='내용을 입력해주세요.' />
+							<textarea name='message' id='' cols='100' rows='5' placeholder='내용을 입력해주세요.' ref={inputMsg} />
 						</div>
 						<div className='formBottomWrap'>
-							<input type='submit' value='Send' className='submitBtn' />
-							보내기
+							{Success && <p>메일이 성공적으로 발송되었습니다.</p>} <input type='submit' value='보내기' className='submitBtn' />
 						</div>
 					</form>
 				</div>
