876ded71da55dc1aa9e6cd7bbcfef61ee26a9fd8
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 <dohyun911@hanmail.net>
2023-07-15 21:35:25 +0900

    feature: gallery 데이터 opt추가

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -9,15 +9,26 @@ function Gallery() {
 		const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
 		const key = '08e2b5a2a14d18ff9a849c7109134194';
 		const method_interest = 'flickr.interestingness.getList';
+		const method_user = 'flickr.people.getPhotos';
+		const method_search = 'flickr.photos.search';
 		const num = 20;
-		const url = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
+		//const myId = '168950802@N02';
+		let url = '';
+		if (opt.type === 'interest') url = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
+		if (opt.type === 'search') url = `${baseURL}&api_key=${key}&method=${method_search}&per_page=${num}&tags=${opt.tags}`;
+		if (opt.type === 'user') url = `${baseURL}&api_key=${key}&method=${method_user}&per_page=${num}&user_id=${opt.user}`;
 
 		const result = await axios.get(url);
 		console.log(result.data.photos.photo);
 		setItems(result.data.photos.photo);
 	};
 
-	useEffect(() => getFlickr({ type: 'interest' }), []);
+	//아래 호출문으로 풍경이미지 검색되도록 함수 코드 수정
+	//getFlickr({type: 'search', tags: 'landscape'})
+
+	//아래 호출문으로 내 계정의 이미지 갤러리 호출되도록
+	//getFlickr({type: 'user', user: '내아이디'})
+	useEffect(() => getFlickr({ type: 'user', user: '168950802@N02' }), []);
 
 	return (
 		<Layout name1={'gallery'} name2={'갤러리'} video={'pexels.mp4'}>
