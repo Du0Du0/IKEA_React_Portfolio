fd9b57dc7f0712dcd4ad8b28db6e265914b30e36
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 <dohyun911@hanmail.net>
2023-07-15 21:31:25 +0900

    feature: gallery 데이터 fetching함수 재사용 가능하도록 async await으로 변경

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -3,20 +3,22 @@ import Layout from '../common/Layout';
 import axios from 'axios';
 
 function Gallery() {
-	const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
-	const key = '08e2b5a2a14d18ff9a849c7109134194';
-	const method_interest = 'flickr.interestingness.getList';
-	const num = 20;
-	const url = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
-
 	const [Items, setItems] = useState([]);
 
-	useEffect(() => {
-		axios.get(url).then((json) => {
-			console.log(json.data.photos.photo);
-			setItems(json.data.photos.photo);
-		});
-	}, []);
+	const getFlickr = async (opt) => {
+		const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
+		const key = '08e2b5a2a14d18ff9a849c7109134194';
+		const method_interest = 'flickr.interestingness.getList';
+		const num = 20;
+		const url = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
+
+		const result = await axios.get(url);
+		console.log(result.data.photos.photo);
+		setItems(result.data.photos.photo);
+	};
+
+	useEffect(() => getFlickr({ type: 'interest' }), []);
+
 	return (
 		<Layout name1={'gallery'} name2={'갤러리'} video={'pexels.mp4'}>
 			<div className='frame'>
