86bfc21bf1f01c3907056bc7d14ba4b558351985
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 <dohyun911@hanmail.net>
2023-07-17 23:37:19 +0900

    feature: 모든 페이지 페이지 이동 시 스크롤 위로 오도록

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
+import ScrollToTop from './components/common/ScrollTop';
 
 ReactDOM.render(
 	<HashRouter>
+		<ScrollToTop />
 		<App />
 	</HashRouter>,
 	document.getElementById('root')
