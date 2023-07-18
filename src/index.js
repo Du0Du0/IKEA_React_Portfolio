01b8bdf43574f6dbd61eceb8e923250d6820d117
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 <dohyun911@hanmail.net>
2023-07-19 01:17:35 +0900

    feature: 전역 store데이터를 루트 컴포넌트인 App에 전달

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
 import ScrollToTop from './components/common/ScrollTop';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 ReactDOM.render(
 	<HashRouter>
-		<ScrollToTop />
-		<App />
+		<Provider store={store}>
+			<ScrollToTop />
+			<App />
+		</Provider>
 	</HashRouter>,
 	document.getElementById('root')
 );
