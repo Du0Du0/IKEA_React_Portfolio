da15e249a85b405b013d91764e96da8ebe78a035
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 <dohyun911@hanmail.net>
2023-07-23 23:38:55 +0900

    feature: Click to open and close the active image, add a function and output a description on vids page

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -1,10 +1,14 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { useSelector } from 'react-redux';
+import { useState, useRef } from 'react';
 
 function Vids() {
 	const MainPageYoutube = useSelector((store) => store.mainYoutubeReducer.mainYoutube);
 	console.log(MainPageYoutube);
+
+	const [SelectedIdx, setSelectedIdx] = useState(0);
+	const videoRefs = useRef(null);
 
 	return (
 		<section id='vids' className='myScroll'>
@@ -26,16 +30,26 @@ function Vids() {
 						// if (idx >= 4) return null;
 
 						return (
-							<div className={`video ${idx === 0 ? 'on' : ''}`} data-video-id={mainVid.snippet.resourceId.videoId} style={{ backgroundImage: `url(${mainVid.snippet.thumbnails.maxres.url})` }}>
-								<div className='vidsNum'>{mainVid.snippet.position + 1}</div>
-								<div className='vidsBarTit'>{(mainVid.snippet.title.length > 15 ? mainVid.snippet.title.substr(0, 20) : mainVid.snippet.title) + '...'}</div>
+							<div
+								className={`video ${SelectedIdx === idx ? 'on' : ''}`}
+								data-video-id={mainVid.snippet.resourceId.videoId}
+								style={{ backgroundImage: `url(${mainVid.snippet.thumbnails.maxres.url})` }}
+								onClick={() => setSelectedIdx(idx)}
+								ref={videoRefs}
+							>
+								<div className='vidsNum' style={{ display: SelectedIdx === idx ? 'none' : 'block' }}>
+									{mainVid.snippet.position + 1}
+								</div>
+								<div className='vidsBarTit' style={{ display: SelectedIdx === idx ? 'none' : 'block' }}>
+									{(mainVid.snippet.title.length > 15 ? mainVid.snippet.title.substr(0, 20) : mainVid.snippet.title) + '...'}
+								</div>
 
-								<span className='vidsOnSpan' style={{ display: 'none' }}>
+								<span className='vidsOnSpan' style={{ display: SelectedIdx === idx ? 'block' : 'none' }}>
 									<h4>{(mainVid.snippet.position + 1).toString().padStart(2, 0)}.</h4>
 									<h2>{(mainVid.snippet.title.length > 50 ? mainVid.snippet.title.substr(0, 50) : mainVid.snippet.title) + '...'}</h2>
 									<p>{(mainVid.snippet.description.length > 50 ? mainVid.snippet.description.substr(0, 250) : mainVid.snippet.description) + '...'}</p>
 								</span>
-								<button className='discoverBtn' style={{ display: 'none' }}>
+								<button className='discoverBtn' style={{ display: SelectedIdx === idx ? 'block' : 'none' }}>
 									Discover Now
 								</button>
 							</div>
