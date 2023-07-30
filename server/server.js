718c970ab2e12560b6903416bc5c9a5dc152a2f0
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 <dohyun911@hanmail.net>
2023-07-30 16:39:42 +0900

    feature: setting MongoDB

--- server/server.js
+++ server/server.js
@@ -1,23 +1,32 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
+const mongoose = require('mongoose');
 const port = 5000;
 
-//클라리언트로 부터 보내진 데이터를 전달받도록 설정 (body-parser)
+//클라이언트로 부터 보내진 데이터를 전달받도록 설정 (body-parser)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//express에서 client측 build폴더까지의 경로를 static하게 지정
+//express에서 react안쪽 build폴더까지의 경로를 static하게 지정
 app.use(express.static(path.join(__dirname, './client/build')));
 
+app.listen(port, () => {
+	mongoose
+		.connect('mongodb+srv://du0du0:!abcd1234@cluster0.5goswz5.mongodb.net/?retryWrites=true&w=majority')
+
+		//접속 성공시
+		.then(() => console.log(`Server app listening on port ${port} with MongoDB`))
+		//접속 실패시
+		.catch((err) => console.log(err));
+});
+
 app.get('/', (req, res) => {
+	//서버에서 5000포트로 접속하면 static폴더로 지정되어 있는 build안쪽의 index.html을 화면에 내보냄
 	res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 app.get('*', (req, res) => {
+	//어떤 URL에서 접속하더라도 화면이 뜨도록 설정
 	res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
-app.listen(port, () => {
-	console.log(`Server app listening on port ${port}`);
 });
