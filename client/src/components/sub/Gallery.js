823bf1310d1ec228243ba61c5b8ae0b6bd92b28a
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 <dohyun911@hanmail.net>
2023-08-02 05:21:44 +0900

    feature: prevent profiles from moving page when it's user gallery

--- client/src/components/sub/Gallery.js
+++ client/src/components/sub/Gallery.js
@@ -8,6 +8,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faCheck } from '@fortawesome/free-solid-svg-icons';
 
 function Gallery() {
+	const isUser = useRef(true);
+	const searchInput = useRef(null);
 	const path = process.env.PUBLIC_URL;
 	const [Items, setItems] = useState([]);
 	const [Loader, setLoader] = useState(true);
@@ -40,6 +42,15 @@ function Gallery() {
 		if (opt.type === 'user') url = `${baseURL}&api_key=${key}&method=${method_user}&per_page=${num}&user_id=${opt.user}`;
 
 		const result = await axios.get(url);
+		if (result.data.photos.photo.length === 0 || result.data.photos.photo === undefined) {
+			setLoader(false);
+
+			frame.current.classList.add('on');
+			setActiveMyGalleryBtn(true);
+			getFlickr({ type: 'user', user: '168950802@N02' });
+			enableEvent.current = true;
+			return alert('이미지 결과값이 없습니다.');
+		}
 		console.log(result.data.photos.photo);
 		setItems(result.data.photos.photo);
 
@@ -71,15 +82,49 @@ function Gallery() {
 
 	//기존 갤러리 초기화 함수
 	const resetGallery = (e) => {
-		const btns = btnSet.current.querySelectorAll('button');
-		btns.forEach((el) => el.classList.remove('on'));
-		e.target.classList.add('on');
 		enableEvent.current = false;
 		setLoader(true);
 		frame.current.classList.remove('on');
 	};
 
 	useEffect(() => getFlickr({ type: 'user', user: '168950802@N02' }), []);
+
+	const showSearch = (e) => {
+		const tag = searchInput.current.value.trim();
+		if (tag === '') return alert('검색어를 입력하세요.');
+		if (!enableEvent.current) return;
+
+		resetGallery(e);
+		getFlickr({ type: 'search', tags: tag });
+		searchInput.current.value = '';
+		isUser.current = false;
+	};
+
+	const showInterest = (e) => {
+		setActiveMyGalleryBtn(true);
+		//재이벤트, 모션중 재이벤트 방지
+		if (!enableEvent.current) return;
+		if (e.target.classList.contains('on')) return;
+
+		//기존 갤러리 초기화 함수 호출
+		resetGallery(e);
+
+		//새로운 데이터로 갤러리 생성 함수 호출
+		getFlickr({ type: 'interest' });
+		isUser.current = false;
+	};
+
+	const showMine = (e) => {
+		setActiveMyGalleryBtn(true);
+		//재이벤트, 모션중 재이벤트 방지
+		if (!enableEvent.current) return;
+
+		//기존 갤러리 초기화 함수 호출
+		resetGallery(e);
+
+		//새로운 데이터로 갤러리 생성 함수 호출
+		getFlickr({ type: 'user', user: '168950802@N02' });
+	};
 
 	return (
 		<>
@@ -89,8 +134,8 @@ function Gallery() {
 			<Layout type={'yellow'} name1={'gallery'} name2={`갤러리`} video={'productsFigure.mp4'}>
 				<div className='searchBarWrap'>
 					<div className='searchBox'>
-						<input type='text' id='search' placeholder='찾으시는 검색어를 입력해 주세요' autoFocus={true} />
-						<button className='btnSearch'>
+						<input type='text' id='search' placeholder='찾으시는 검색어를 입력해 주세요' autoFocus={true} ref={searchInput} onKeyPress={(e) => e.key === 'Enter' && showSearch(e)} />
+						<button className='btnSearch' onClick={showSearch}>
 							<p>검색</p>
 						</button>
 					</div>
@@ -102,11 +147,11 @@ function Gallery() {
 								setActiveMyGalleryBtn(false);
 								//재이벤트, 모션중 재이벤트 방지
 								if (!enableEvent.current) return;
-								if (e.target.classList.contains('on')) return;
 
 								//기존 갤러리 초기화 함수 호출
 								resetGallery(e);
 								getFlickr({ type: 'interest' });
+								isUser.current = false;
 							}}
 							style={ActiveMyGalleryBtn === false ? activeGalleryBtnStyle : null}
 						>
@@ -123,11 +168,10 @@ function Gallery() {
 								setActiveMyGalleryBtn(true);
 								//재이벤트, 모션중 재이벤트 방지
 								if (!enableEvent.current) return;
-								if (e.target.classList.contains('on')) return;
 
 								//기존 갤러리 초기화 함수 호출
 								resetGallery(e);
-								getFlickr({ type: 'user', user: '164021883@N04' });
+								getFlickr({ type: 'user', user: '168950802@N02' });
 							}}
 							style={ActiveMyGalleryBtn === true ? activeGalleryBtnStyle : null}
 						>
@@ -167,6 +211,8 @@ function Gallery() {
 											{/* 프로필 사진 클릭 시  해당 유저 페이지로 이동 */}
 											<span
 												onClick={(e) => {
+													if (isUser.current) return;
+													isUser.current = true;
 													setLoader(true);
 													frame.current.classList.remove('on');
 													getFlickr({ type: 'user', user: e.target.innerText });
