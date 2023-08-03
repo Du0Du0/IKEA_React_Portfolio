19c17760ea41214a300f390418cb984562b8e0eb
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 <dohyun911@hanmail.net>
2023-08-03 18:44:43 +0900

    feature: add flickr toolkit settings

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,18 @@
 import ReactDOM from 'react-dom';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-
-import { configureStore } from '@reduxjs/toolkit';
-import mainYoutubeReducer from './redux-toolkit/mainYoutubeSlice';
 import ScrollToTop from './components/common/ScrollTop';
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import mainYoutubeReducer from './redux-toolkit/mainYoutubeSlice';
+import flickrReducer from './redux/flickrSlice';
 
 const store = configureStore({
 	reducer: {
 		mainYoutube: mainYoutubeReducer,
+		flickr: flickrReducer,
 	},
 });
 
---
+++ client/src/redux-toolkit/flickerSlice.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const fetchFlickr = createAsyncThunk('flickr/requestFlickr', async (opt) => {
+	const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
+	const key = '08e2b5a2a14d18ff9a849c7109134194';
+	const method_interest = 'flickr.interestingness.getList';
+	const method_user = 'flickr.people.getPhotos';
+	const method_search = 'flickr.photos.search';
+	const num = 50;
+	let url = '';
+
+	if (opt.type === 'interest') url = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
+	if (opt.type === 'search') url = `${baseURL}&api_key=${key}&method=${method_search}&per_page=${num}&tags=${opt.tags}`;
+	if (opt.type === 'user') url = `${baseURL}&api_key=${key}&method=${method_user}&per_page=${num}&user_id=${opt.user}`;
+
+	const response = await axios.get(url);
+	return response.data.photos.photo;
+});
+
+const flickrSlice = createSlice({
+	name: 'flickr',
+	initialState: {
+		data: [],
+		isLoading: false,
+	},
+	extraReducers: {
+		[fetchFlickr.pending]: (state) => {
+			state.isLoading = true;
+		},
+		[fetchFlickr.fulfilled]: (state, action) => {
+			state.isLoading = false;
+			state.data = action.payload;
+		},
+		[fetchFlickr.rejected]: (state, action) => {
+			state.isLoading = false;
+			state.data = action.payload;
+		},
+	},
+});
+
+export default flickrSlice.reducer;
